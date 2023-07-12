9e9dd14982ad8e49ab60dfbe3a615fcc29fc9090
easterNday <849919718@qq.com>
2023-07-12 13:44:30 +0800

    Progress: Show progressbar while downloading
    * Add progreebar to show download progress
    * Add some comments

--- Settings/app/src/main/java/top/easterNday/settings/DogDay/Utils.kt
+++ Settings/app/src/main/java/top/easterNday/settings/DogDay/Utils.kt
@@ -1,15 +1,12 @@
 package top.easterNday.settings.DogDay
 
-import android.annotation.SuppressLint
 import android.app.AlertDialog
-import android.app.DownloadManager
-import android.app.DownloadManager.Query
 import android.content.ClipData
 import android.content.ClipboardManager
 import android.content.Context
+import android.content.Intent
 import android.net.Uri
-import android.os.Environment
-import androidx.core.net.toFile
+import android.os.RecoverySystem
 import java.io.File
 import java.text.SimpleDateFormat
 import java.util.*
@@ -27,10 +24,10 @@ class Utils {
 
     companion object {
         /**
-         * The function `toDate` converts a timestamp in seconds to a formatted date and time string in the format "yyyy-MM-dd HH:mm".
+         * 该函数将 Unix 时间戳转换为格式为“yyyy-MM-dd HH:mm:ss”的本地时间字符串。
          *
-         * @param timestamp The `timestamp` parameter is a double value representing the number of seconds since the Unix epoch (January 1, 1970, 00:00:00 UTC).
-         * @return The function `toDate` returns a string representation of a date and time in the format "yyyy-MM-dd HH:mm".
+         * @param unixTimeStamp `unixTimeStamp` 参数是一个长值，表示自 1970 年 1 月 1 日 00:00:00 UTC（协调世界时）以来经过的秒数。
+         * @return 表示与给定 Unix 时间戳对应的本地时间的格式化字符串。
          */
         fun convertUnixToLocalTime(unixTimeStamp: Long): String {
             val date = Date(unixTimeStamp * 1000) // 将 Unix 时间戳转换为毫秒
@@ -39,69 +36,11 @@ class Utils {
             return sdf.format(date) // 格式化为当地时间字符串
         }
 
-        fun genDownloadPath(subdir: String, filename: String): Uri {
-            //设置文件存放目录
-            val destinationDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
-            val destinationPath = File(destinationDirectory, subdir)
-            if (!destinationPath.exists()) {
-                destinationPath.mkdirs()
-            }
-            val destinationFile = File(destinationPath, filename)
-            return Uri.fromFile(destinationFile)
-        }
-
-        @SuppressLint("Range")
-        fun isFileDownloadedAndAvailable(context: Context, url: String, path: Uri): Boolean {
-            val downloadManager = context.getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
-            val query = Query()
-            query.setFilterByStatus(DownloadManager.STATUS_SUCCESSFUL)
-            val cursor = downloadManager.query(query)
-            if (cursor.moveToFirst()) {
-                do {
-                    val downloadedUrl = cursor.getString(cursor.getColumnIndex(DownloadManager.COLUMN_URI))
-                    if (downloadedUrl == url) {
-                        val file = path.toFile()
-                        return file.exists() // 文件存在，表示已下载且未被删除
-                    }
-                } while (cursor.moveToNext())
-            }
-            return false // 文件未下载或已被删除
-        }
-
         /**
-         * The function `downloadFromUrl` downloads a file from a given URL and saves it to the specified directory on the device.
+         * 函数“copyLink2Clipboard”将给定的 URL 复制到 Kotlin 中的剪贴板。
          *
-         * @param context The context parameter is the current context of the application. It is typically passed as the activity or fragment where the download is initiated.
-         * @param downloadUrl The URL from which the file needs to be downloaded.
-         * @param filename The name of the file to be downloaded.
-         * @param desc The `desc` parameter is a description of the file being downloaded. It is used to provide additional information about the download in the notification.
-         * @param subdir The `subdir` parameter is a string that represents the subdirectory where the downloaded file will be stored. It is used to create a directory within the Downloads directory on the device's external storage.
-         * @return The function `downloadFromUrl` returns a `Long` value, which is the unique ID of the download request that was enqueued in the `DownloadManager`.
-         */
-        fun downloadFromUrl(
-            context: Context,
-            downloadUrl: String,
-            filename: String,
-            desc: String,
-            path: Uri
-        ): Long {
-            val request: DownloadManager.Request = DownloadManager.Request(Uri.parse(downloadUrl))
-            // 设置在什么网络情况下进行下载
-            request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_MOBILE or DownloadManager.Request.NETWORK_WIFI)
-            //设置通知栏标题
-            request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
-            request.setTitle(filename)
-            request.setDescription(desc)
-            request.setDestinationUri(path)
-            val downloadManager = context.getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
-            return downloadManager.enqueue(request)
-        }
-
-        /**
-         * The function `copyLink2Clipboard` copies a given URL to the clipboard in Kotlin.
-         *
-         * @param context The context parameter is the current context of the application. It is typically passed as the activity or fragment where the method is being called from. It is used to access system services and resources.
-         * @param url The URL that you want to copy to the clipboard.
+         * @param context context 参数是应用程序的当前上下文。它通常作为调用方法的活动或片段传递。它用于访问系统服务和资源。
+         * @param url 您要复制到剪贴板的 URL。
          */
         fun copyLink2Clipboard(context: Context, url: String) {
             //获取剪贴板管理器：
@@ -113,12 +52,13 @@ class Utils {
             cm.setPrimaryClip(mClipData)
         }
 
+
         /**
-         * The function `showAlertDialog` displays an alert dialog with a given title and content in Kotlin.
+         * 函数“showAlertDialog”在 Kotlin 中显示具有给定标题和内容的警报对话框。
          *
-         * @param context The context parameter is the current context of the application. It is typically passed as the activity or fragment where the dialog is being shown.
-         * @param title The title parameter is a string that represents the title of the alert dialog. It is displayed at the top of the dialog box.
-         * @param content The `content` parameter is a string that represents the message or content of the alert dialog. It is the text that will be displayed to the user.
+         * @param context context 参数是应用程序的当前上下文。它通常作为显示对话框的活动或片段传递。
+         * @param title title 参数是一个字符串，表示警报对话框的标题。它显示在对话框的顶部。
+         * @param content `content` 参数是一个字符串，表示警报对话框的消息或内容。它是将显示给用户的文本。
          */
         fun showAlertDialog(context: Context, title: String, content: String) {
             val builder: AlertDialog.Builder = AlertDialog.Builder(context)
@@ -126,6 +66,28 @@ class Utils {
             builder.setMessage(content)
             builder.setPositiveButton(android.R.string.ok) { dialog, _ -> dialog.dismiss() }
             builder.show()
+        }
+
+        /**
+         * 函数 `openUrl` 使用 Kotlin 中的意图在 Web 浏览器中打开 URL。
+         *
+         * @param context context 参数是 Context 类的实例，它提供对特定于应用程序的资源和类的访问。它通常从活动或片段传入。
+         * @param uri `uri` 参数是一个字符串，表示您要打开的 URL 或 URI。它可以是 Web URL、文件 URI 或任何其他有效的 URI 方案。
+         */
+        fun openUrl(context: Context, uri: String) {
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse(uri)
+            context.startActivity(intent)
+        }
+
+        fun Context.installPackage(update: Uri) {
+            val file = File(update.path.toString())
+            if (file.exists()) {
+                RecoverySystem.installPackage(this, file)
+            }
+            else{
+                showAlertDialog(this,"错误","没有下载对应文件或对应文件损坏!")
+            }
         }
 
     }
--- Settings/app/src/main/java/top/easterNday/settings/Update/UpdatesListAdapter.kt
+++ Settings/app/src/main/java/top/easterNday/settings/Update/UpdatesListAdapter.kt
@@ -1,45 +1,31 @@
 package top.easterNday.settings.Update
 
 
-/*
- * Copyright (C) 2017-2023 The LineageOS Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 import android.annotation.SuppressLint
+import android.app.Activity
+import android.app.DownloadManager
 import android.content.Context
-import android.os.RecoverySystem
+import android.net.Uri
+import android.os.Environment
+import android.text.format.Formatter
 import android.view.LayoutInflater
 import android.view.MenuInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.ImageButton
-import android.widget.TextView
+import android.widget.*
 import androidx.appcompat.widget.PopupMenu
-import androidx.core.net.toFile
+import androidx.core.net.toUri
 import androidx.recyclerview.widget.RecyclerView
-import top.easterNday.settings.DogDay.LogUtils.logger
 import top.easterNday.settings.DogDay.Utils.Companion.copyLink2Clipboard
-import top.easterNday.settings.DogDay.Utils.Companion.downloadFromUrl
-import top.easterNday.settings.DogDay.Utils.Companion.genDownloadPath
-import top.easterNday.settings.DogDay.Utils.Companion.isFileDownloadedAndAvailable
+import top.easterNday.settings.DogDay.Utils.Companion.installPackage
 import top.easterNday.settings.DogDay.Utils.Companion.showAlertDialog
 import top.easterNday.settings.R
+import java.io.File
+import java.util.*
 
 
-class UpdatesListAdapter(private val mContext: Context, private val dataSet: ArrayList<UpdateItem>) :
+class
+UpdatesListAdapter(private val mContext: Context, private val dataSet: ArrayList<UpdateItem>) :
     RecyclerView.Adapter<UpdatesListAdapter.ViewHolder>() {
     /**
      * Provide a reference to the type of views that you are using
@@ -51,9 +37,13 @@ class UpdatesListAdapter(private val mContext: Context, private val dataSet: Arr
         val datetime: TextView
         val size: TextView
         val tag: TextView
-        val desc: TextView
         val actionButton: Button
+
         val mMenu: ImageButton
+        val mProgress: LinearLayout
+        val mProgressBar: ProgressBar
+        val mProgressBarPercent: TextView
+        val mProgressText: TextView
 
         init {
             // Define click listener for the ViewHolder's View.
@@ -62,10 +52,14 @@ class UpdatesListAdapter(private val mContext: Context, private val dataSet: Arr
             datetime = view.findViewById(R.id.updateDate)
             size = view.findViewById(R.id.updateSize)
             tag = view.findViewById(R.id.updateTag)
-            desc = view.findViewById(R.id.updateDesc)
             actionButton = view.findViewById(R.id.updateAction)
             // Action Button
             mMenu = view.findViewById(R.id.updateMenu)
+            // ProcessBar
+            mProgress = view.findViewById(R.id.update_progress_container)
+            mProgressBar = view.findViewById(R.id.update_progress_bar)
+            mProgressBarPercent = view.findViewById(R.id.update_progress_percent)
+            mProgressText = view.findViewById(R.id.update_progress_text)
         }
     }
 
@@ -80,54 +74,25 @@ class UpdatesListAdapter(private val mContext: Context, private val dataSet: Arr
     // Return the size of your dataset (invoked by the layout manager)
     override fun getItemCount() = dataSet.size
 
-    // Replace the contents of a view (invoked by the layout manager)
     @SuppressLint("RestrictedApi")
     override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
-
-        // Get element from your dataset at this position and replace the
-        // contents of the view with that element
-        val download_url = dataSet[position].updateUrl
-        val update_log = dataSet[position].updateDesc
+        val downloadUrl = dataSet[position].updateUrl
+        val updateLog = dataSet[position].updateDesc
         val filename = dataSet[position].updateTitle
-        val fileUri = genDownloadPath(mContext.getString(R.string.download_sub_dir), filename)
 
         viewHolder.title.text = filename
         viewHolder.version.text = dataSet[position].updateVersion
         viewHolder.datetime.text = dataSet[position].updateDate
         viewHolder.size.text = dataSet[position].updateSize
-        viewHolder.desc.text = update_log
         viewHolder.tag.text = dataSet[position].updateTag
 
-
+        // 绑定 Popup 菜单功能
         viewHolder.mMenu.setOnClickListener {
-            logger.d("PopUp！")
-            showPopup(mContext, viewHolder.mMenu, download_url, update_log)
+            viewHolder.showPopup(downloadUrl, updateLog)
         }
 
-        if (isFileDownloadedAndAvailable(mContext, download_url, fileUri)) {
-            viewHolder.actionButton.text = "刷入"
-
-
-            viewHolder.actionButton.setOnClickListener {
-                val file = fileUri.toFile()
-                logger.d("Install %s", file.toString())
-                RecoverySystem.installPackage(mContext, file)
-            }
-        } else {
-            viewHolder.actionButton.text = "下载"
-            // 点击下载按钮开始下载
-            viewHolder.actionButton.setOnClickListener {
-                // 调用系统服务进行下载
-                downloadFromUrl(
-                    mContext,
-                    download_url,
-                    filename,
-                    dataSet[position].updateDesc,
-                    fileUri
-                )
-            }
-        }
-
+        // 设定下载显示
+        viewHolder.setDownload(downloadUrl, filename)
     }
 
     fun addUpdateItem(item: UpdateItem) {
@@ -137,8 +102,8 @@ class UpdatesListAdapter(private val mContext: Context, private val dataSet: Arr
         notifyItemInserted(dataSet.size - 1) // 获取新添加item的位置
     }
 
-    private fun showPopup(c: Context, v: View, url: String, log: String) {
-        val popup = PopupMenu(c, v)
+    private fun ViewHolder.showPopup(url: String, log: String) {
+        val popup = PopupMenu(mContext, mMenu)
         val inflater: MenuInflater = popup.menuInflater
         inflater.inflate(R.menu.menu_update_item, popup.menu)
 
@@ -147,22 +112,170 @@ class UpdatesListAdapter(private val mContext: Context, private val dataSet: Arr
             when (menuItem.itemId) {
                 R.id.menu_copy_url -> {
                     // 复制内容到剪贴板
-                    copyLink2Clipboard(c, url)
-                    // 打开网页
-                    // val intent = Intent(Intent.ACTION_VIEW)
-                    // intent.data = Uri.parse(dataSet[position].updateUrl)
-                    // viewHolder.itemView.context.startActivity(intent)
+                    copyLink2Clipboard(mContext, url)
                     true
                 }
 
                 R.id.menu_update_log -> {
-                    showAlertDialog(c, c.getString(R.string.text_menu_update_log), log)
+                    showAlertDialog(mContext, mContext.getString(R.string.text_menu_update_log), log)
                     true
+                }
+
+                R.id.menu_delete_action -> {
+                    TODO()
                 }
 
                 else -> false
             }
         }
         popup.show()
+    }
+
+    // 控制进度部分和大小部分的显示
+    private fun ViewHolder.showProgress(flag: Boolean) {
+        val visibility = if (flag) View.VISIBLE else View.INVISIBLE
+
+        size.visibility = if (flag) View.INVISIBLE else View.VISIBLE
+        mProgress.visibility = visibility
+        mProgressBar.visibility = visibility
+        mProgressBarPercent.visibility = visibility
+        mProgressText.visibility = visibility
+    }
+
+    private fun ViewHolder.onActionButtonClick(action: () -> Unit) {
+        actionButton.setOnClickListener(null)
+        actionButton.setOnClickListener {
+            action.invoke()
+        }
+    }
+
+    private fun ViewHolder.setDownload(downloadUrl: String, filename: String) {
+
+        fun getDownloadUri(): Uri {
+            val romsDir = File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), "Roms")
+            if (!romsDir.exists()) {
+                romsDir.mkdirs()
+            }
+            val update = File(romsDir.path, filename)
+            return update.toUri()
+        }
+
+        val downloadManager = mContext.getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
+        val downloadRequest = DownloadManager.Request(Uri.parse(downloadUrl))
+        // 设置在什么网络情况下进行下载
+        downloadRequest.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_MOBILE or DownloadManager.Request.NETWORK_WIFI)
+        //设置通知栏标题
+        downloadRequest.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
+        // request.setTitle(filename)
+        // request.setDescription(desc)
+        // 设置下载目录为系统的下载目录
+        //downloadRequest.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, filename)
+        downloadRequest.setDestinationUri(getDownloadUri())
+
+        var downloadId: Long = -1 // 用于保存下载任务的ID
+
+
+        // 更新下载按钮的文本
+        fun updateActionButtonText(text: String) {
+            actionButton.text = text
+        }
+
+        // 更新按钮点击事件
+        fun updateActionButtonClick(action: () -> Unit) {
+            actionButton.setOnClickListener(null)
+            actionButton.setOnClickListener {
+                action.invoke()
+            }
+        }
+
+        // 暂停下载任务
+        fun pauseDownload() {
+            downloadManager.remove(downloadId)
+            updateActionButtonText(mContext.getString(R.string.update_download))
+        }
+
+
+        // 开始下载任务
+        fun startDownload() {
+            downloadId = downloadManager.enqueue(downloadRequest)
+            updateActionButtonText(mContext.getString(R.string.update_downloading))
+        }
+
+        // 查询下载任务的状态和进度
+        fun queryDownloadStatus() {
+            val query = DownloadManager.Query()
+            query.setFilterById(downloadId)
+
+            val cursor = downloadManager.query(query)
+            if (cursor.moveToFirst()) {
+                val status = cursor.getInt(cursor.getColumnIndexOrThrow(DownloadManager.COLUMN_STATUS))
+                val progress =
+                    cursor.getInt(cursor.getColumnIndexOrThrow(DownloadManager.COLUMN_BYTES_DOWNLOADED_SO_FAR))
+                val total = cursor.getInt(cursor.getColumnIndexOrThrow(DownloadManager.COLUMN_TOTAL_SIZE_BYTES))
+
+                when (status) {
+                    DownloadManager.STATUS_RUNNING, DownloadManager.STATUS_PENDING -> {
+                        showProgress(true)
+                        val percent = progress * 100 / total
+                        mProgressBar.progress = percent
+                        mProgressBarPercent.text = mContext.getString(R.string.text_progress, percent)
+                        mProgressText.text = mContext.getString(
+                            R.string.text_progress_total,
+                            Formatter.formatFileSize(mContext, progress.toLong()),
+                            Formatter.formatFileSize(mContext, total.toLong())
+                        )
+
+                        // 下载暂停，显示继续按钮
+                        updateActionButtonText(mContext.getString(R.string.update_pause))
+                        updateActionButtonClick {
+                            pauseDownload()
+                        }
+                    }
+
+                    DownloadManager.STATUS_PAUSED -> {
+                        // 下载暂停，显示继续按钮
+                        updateActionButtonText(mContext.getString(R.string.update_resume))
+                        updateActionButtonClick {
+                            startDownload()
+                        }
+                    }
+
+                    DownloadManager.STATUS_SUCCESSFUL -> {
+                        // 下载完成，显示安装按钮或其他操作
+                        showProgress(false)
+                        updateActionButtonText(mContext.getString(R.string.update_flash))
+                        updateActionButtonClick {
+                            mContext.installPackage(getDownloadUri())
+                        }
+                    }
+
+                    DownloadManager.STATUS_FAILED -> {
+                        // 下载失败，显示重新下载按钮
+                        updateActionButtonText(mContext.getString(R.string.update_download))
+                        updateActionButtonClick {
+                            startDownload()
+                        }
+                    }
+                }
+            } else {
+                updateActionButtonText(mContext.getString(R.string.update_download))
+                updateActionButtonClick {
+                    startDownload()
+                }
+            }
+
+            cursor.close()
+        }
+
+        // 定时查询下载状态
+        val timer = Timer()
+        val timerTask = object : TimerTask() {
+            override fun run() {
+                (mContext as Activity).runOnUiThread {
+                    queryDownloadStatus()
+                }
+            }
+        }
+        timer.schedule(timerTask, 0, 1000) // 每隔1秒查询一次下载状态
     }
 }
